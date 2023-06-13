413df9f636f71a57890b35e781f556fea8cfd6e6
TRDaybreak <49009060+TRDaybreak@users.noreply.github.com>
2023-06-13 14:28:49 -0700

    First working example of QFFormula

--- incompleteness/SigmaDefinability.lean
+++ incompleteness/SigmaDefinability.lean
@@ -1,16 +1,16 @@
 import Mathlib.ModelTheory.Syntax
+
 open FirstOrder
 open FirstOrder.Language
 
+namespace FirstOrder.Language
 
 universe u v w u' v'
 
-variable (L : FirstOrder.Language) (α : Type u') (n : ℕ)
-
-#check (BoundedFormula.imp)
+-- variable (L : FirstOrder.Language) (α : Type u') (n : ℕ)
 
 
-inductive  QFFormula : (L.BoundedFormula α n) →  Prop
+inductive  QFFormula  {L : Language} {α : Type u'} {n : ℕ} : (L.BoundedFormula α n) →  Prop
   | falsum : QFFormula (falsum)
   | equal (t₁ t₂ : L.Term (Sum α (Fin n))) : QFFormula (equal)
   | rel (R : L.Relations l) (ts : Fin l → L.Term (Sum α (Fin n))) : QFFormula (rel)
@@ -19,11 +19,12 @@ inductive  QFFormula : (L.BoundedFormula α n) →  Prop
 -- Test
 example : Prop := by
   have L0 := FirstOrder.Language.empty
-  have t0 : (Language.Term L0 ℕ) := Language.Term.var 0
-  -- have phi: (Formula) := L0.BoundedFormula.falsum 0
-
-  -- have : QFFormula ((L0.BoundedFormula α 0).falsum)
+  have t0 : (Term L0 (ℕ ⊕ Fin 0)) := Language.Term.var (Sum.inl 0)
+  have f0 : (Formula L0 ℕ) := BoundedFormula.equal t0 t0
+  have qf0 : (QFFormula f0) := by
+    apply QFFormula.equal t0 t0
 
   exact True
 
-  #check Language
+#check BoundedFormula.equal
+#check Fin 0
