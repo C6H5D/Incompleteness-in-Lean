1215c87ccd826c1aa41e1d34037f625ebcf8bf96
TRDaybreak <49009060+TRDaybreak@users.noreply.github.com>
2023-06-13 22:02:34 -0700

    Added Type Aliases, and Coercion from number to ArithmeticTerm

--- incompleteness/Arithmetic.lean
+++ incompleteness/Arithmetic.lean
@@ -1,4 +1,5 @@
 import Mathlib.ModelTheory.Basic
+import Mathlib.ModelTheory.Syntax
 
 namespace FirstOrder
 namespace Language
@@ -35,8 +36,6 @@ protected def times : (Arithmetic.L_arithmetic.Functions 2) := BinaryFunctions.t
 
 protected def le : (Arithmetic.L_arithmetic.Relations 2) := Relations.le
 
-#check Language.Structure.mk₂
-
 def true_arithmetic : Arithmetic.L_arithmetic.Structure ℕ := by
   apply Language.Structure.mk₂
   <;> intro a
@@ -46,3 +45,68 @@ def true_arithmetic : Arithmetic.L_arithmetic.Structure ℕ := by
   . exact Nat.add
   . exact Nat.mul
   . exact Nat.le
+
+-- Type Notation Simplifications
+@[reducible]
+def BoundedArithmeticFormula (n : ℕ) :=
+  BoundedFormula L_arithmetic ℕ n
+
+@[reducible]
+def ArithmeticFormula :=
+  Formula L_arithmetic ℕ
+
+example: ArithmeticFormula = BoundedArithmeticFormula 0 := rfl
+
+@[reducible]
+def BoundedArithmeticTerm (n : ℕ) :=
+  Term L_arithmetic (ℕ ⊕ Fin n)
+
+@[reducible]
+def ArithmeticTerm := BoundedArithmeticTerm 0
+
+-- Operation Simplifications
+instance : Coe ℕ ArithmeticTerm where
+  coe := by
+    intro n
+    induction' n with _ IH
+    . exact Constants.term Arithmetic.zero
+    . exact Functions.apply₁ Arithmetic.succ IH
+
+-----------------Recall from ModelTheory.Syntax:-----------------
+
+-- prefix:arg "&" => FirstOrder.Language.Term.var ∘ Sum.inr
+--   -- so &n is a term for n-th free variable in BoundedFormula
+
+-- scoped[FirstOrder] infixl:88 " =' " => FirstOrder.Language.Term.bdEqual
+-- -- input \~- or \simeq
+
+-- scoped[FirstOrder] infixr:62 " ⟹ " => FirstOrder.Language.BoundedFormula.imp
+-- -- input \==>
+
+-- scoped[FirstOrder] prefix:110 "∀'" => FirstOrder.Language.BoundedFormula.all
+
+-- scoped[FirstOrder] prefix:arg "∼" => FirstOrder.Language.BoundedFormula.not
+-- -- input \~, the ASCII character ~ has too low precedence
+
+-- scoped[FirstOrder] infixl:61 " ⇔ " => FirstOrder.Language.BoundedFormula.iff
+-- -- input \<=>
+
+-- scoped[FirstOrder] prefix:110 "∃'" => FirstOrder.Language.BoundedFormula.ex
+-- -- input \ex
+
+-- instance : Top (L.BoundedFormula α n) :=
+--   ⟨BoundedFormula.not ⊥⟩
+  -- use by ⊥ of corresponding type
+
+-- instance : Bot (L.BoundedFormula α n) :=
+--   ⟨falsum⟩
+  -- use by ⊤ of corresponding type
+
+-- instance : Inf (L.BoundedFormula α n) :=
+--   ⟨fun f g => (f.imp g.not).not⟩
+  -- use ⊓ 
+
+-- instance : Sup (L.BoundedFormula α n) :=
+-- ⟨fun f g => f.not.imp g⟩
+  -- use ⊔ 
+
