62897aa1fce224f3d2a1a8fb608afc0eff5674ed
TRDaybreak <49009060+TRDaybreak@users.noreply.github.com>
2023-06-13 20:07:32 -0700

    Beginning of Arithmetic branch

--- incompleteness/Arithmetic.lean
+++ incompleteness/Arithmetic.lean
@@ -1,18 +1,19 @@
 import Mathlib.ModelTheory.Basic
 namespace FirstOrder
 namespace Language
+namespace Arithmetic
 
-inductive Arithmetic.Constants : Type
+inductive Constants : Type
   | zero
 
-inductive Arithmetic.BinaryFunctions : Type
+inductive UnaryFunctions : Type
+  | succ
+
+inductive BinaryFunctions : Type
   | plus
   | times
 
-inductive Arithmetic.UnaryFunctions : Type
-  | succ
-
-inductive Arithmetic.Relations : Type
+inductive Relations : Type
   | le
 
 -- export Arithmetic.Constants (zero)
@@ -20,12 +21,22 @@ inductive Arithmetic.Relations : Type
 -- export Arithmetic.UnaryFunctions (succ)
 -- export Arithmetic.Relations (le)
 
-protected def arithmetic : Language :=
+def L_arithmetic : Language :=
   Language.mk₂ Arithmetic.Constants Arithmetic.UnaryFunctions Arithmetic.BinaryFunctions Empty Arithmetic.Relations
+
+protected def zero : (Arithmetic.L_arithmetic.Constants) := Constants.zero
+
+protected def succ : (Arithmetic.L_arithmetic.Functions 1) := UnaryFunctions.succ
+
+protected def plus : (Arithmetic.L_arithmetic.Functions 2) := BinaryFunctions.plus
+
+protected def times : (Arithmetic.L_arithmetic.Functions 2) := BinaryFunctions.times
+
+protected def le : (Arithmetic.L_arithmetic.Relations 2) := Relations.le
 
 #check Language.Structure.mk₂
 
-def true_arithmetic : Language.arithmetic.Structure ℕ := by
+def true_arithmetic : Arithmetic.L_arithmetic.Structure ℕ := by
   apply Language.Structure.mk₂
   <;> intro a
   <;> cases' a
