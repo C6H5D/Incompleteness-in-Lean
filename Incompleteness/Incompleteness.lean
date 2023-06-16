ccc5a206dbc95d34bd1c09cf4b750fb068314f24
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2023-06-16 16:31:40 -0700

    Making proof even shorter.

--- Incompleteness/Incompleteness.lean
+++ Incompleteness/Incompleteness.lean
@@ -52,9 +52,7 @@ theorem part_rec_implies_sigma_one_definable {f : ℕ →. ℕ} {hf : Nat.Partre
           apply BoundedFormula.IsQF.of_isAtomic
           apply BoundedFormula.IsAtomic.equal
         . intro m n
-          have meaningFormula : ψ_succ.Realize default ![m, n] ↔ Nat.succ m = n := by
-            rfl
-          rw [meaningFormula]
+          rw [ψ_succ, x0, x1]
           symm
           exact PartENat.natCast_inj
     | left => 
